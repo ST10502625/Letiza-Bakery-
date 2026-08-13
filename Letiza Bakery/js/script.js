ec2930a604eb711225106c401305354d203ebda9
ST10502625 <ST10502625@RCCONNECT.EDU.ZA>
2026-08-14 00:31:13 +0200

    Add files via upload

---
+++ Letiza Bakery/js/script.js
@@ -0,0 +1,5 @@
+// script.js
+// Letiza Bakery - JavaScript functionality
+// Part 1: Placeholder only. Form validation and interactivity to be built in Part 3.
+
+console.log("Letiza Bakery site loaded");
